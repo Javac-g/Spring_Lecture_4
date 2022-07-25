e6b208c908884d0b342ed4bfab153ef724a29d21
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-25 18:11:53 +0300

    Service B

---
+++ Service/Service_B.java
@@ -0,0 +1,11 @@
+package com.company.Service;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Service_B {
+    @Value("${prop.B}")
+    private String one;
+    public String getOne(){
+        return one;
+    }
+}
