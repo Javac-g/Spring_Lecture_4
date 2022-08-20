d169d9ec885a2e0d807db18394f51fec05cb4bec
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-08-20 21:07:00 +0300

    next

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,26 +1,33 @@
 package com.company.Controller;
 
 import com.company.Service.Service;
-import com.company.Service.Service_A;
-import com.company.Service.Service_B;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @RequestMapping("/Main")
 public class Controller {
     private final static Logger log = LoggerFactory.getLogger(Controller.class);
 
     @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired              // Direct mapping by value
     @Qualifier("Service_B")
+    private Service A;
 
-    private Service services;
-
+    @Autowired        //Mapped by @Primary
+    private Service B;
 
 
 
@@ -28,8 +35,22 @@ public class Controller {
     @ResponseBody
     public String service(){
         log.info("https://192.168.0.104:8080/Main/S");
-        log.info("Service: " + services);
+        log.info("A: " + A);
+        log.info("B: " + B);
 
-        return "\n" + services.getData();
+        return "\n" + A.getData() + "\n" + B.getData();
+    }
+    @PostConstruct
+    public void init(){
+        log.info("PostConstruct initialised");
+    }
+    @PreDestroy
+    public void destroy(){
+        log.info("PreDestroy call");
+
+    }
+    @RequestMapping(value = "/shutdown", method = RequestMethod.GET)
+    private void shutdown(){
+        ((ConfigurableApplicationContext)applicationContext).close();
     }
 }
